24914d58187f6d3e7420cd36b2835b938dc88198
cmcm <1446525549@qq.com>
2021-05-25 00:22:17 +0800

    新增堆排序和改善快速排序

--- DataStructureFace/src/main/java/com/datastructure/other/QuickSort.java
+++ DataStructureFace/src/main/java/com/datastructure/other/QuickSort.java
@@ -12,22 +12,23 @@ public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {1,3,4,2,2};
         quickSort(arr,0,arr.length-1);
-//        Swap.printArr(arr);
+        Swap.printArr(arr);
     }
     public static void quickSort(int[] arr,int L,int R){
-//        if (L < R){
+        if (L < R){
+            Swap.swap(arr,L+(int)Math.random()*(R-L+1),R);//添加上这个就是随机快排，变一个概率性的事件
             int[] partion = partion(arr, L, R);
-            Swap.printArr(partion);
-//            quickSort(arr,L,partion[0]-1);
-//            quickSort(arr,partion[1]+1,R );
-//        }
+//            Swap.printArr(partion);
+            quickSort(arr,L,partion[0]-1);
+            quickSort(arr,partion[1]+1,R );
+        }
 
     }
 
     public static int[] partion(int[] arr,int L,int R){
         int less = L -1;
         int more = R;
-        while (L < R){
+        while (L < more){
             if (arr[L] < arr[R]){
                 Swap.swap(arr,++less,L++);
             } else if (arr[L] > arr[R]) {
@@ -36,6 +37,7 @@ public class QuickSort {
                 L++;
             }
         }
+//        Swap.printArr(arr);
         Swap.swap(arr,more,R);
         return new int[]{less+1,more};
     }
---
+++ DataStructureFace/src/main/java/com/datastructure/sort/HeapSort.java
@@ -0,0 +1,52 @@
+package com.datastructure.sort;
+
+
+/**
+ * 堆排序，要理解完全二叉树，数组和完全二叉树可以相互转换
+ * 转换关系极为2*i+1是当前节点的左孩子 2*i+2是当前节点的右孩子 当前节点的父节点为(i-1)/2
+ */
+public class HeapSort {
+
+    public static void main(String[] args) {
+        int[] arr = {2,1,3,6,0,4};
+        headSort(arr);
+        Swap.printArr(arr);
+    }
+    public static void headSort(int[] arr){
+        if (null == arr || arr.length < 2){
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            insertHeap(arr,i);
+        }
+    }
+
+    /**
+     * 插入进堆里面，如果是降序排序就采用大根堆，大根堆即为当前节点堆的左右孩子都小于当前节点
+     * @param arr
+     * @param index
+     */
+    public static void insertHeap(int[] arr,int index){
+        while (arr[index] > arr[(index-1)/2]){
+            Swap.swap(arr,index,(index-1)/2);
+            index = (index-1)/2;
+        }
+    }
+
+
+    public static void heapify(int[] arr,int index,int heapSize){
+        //得到当前节点的左孩子
+        int left = 2*index +1;
+        while (left < heapSize){
+            int larget = left + 1 <heapSize && arr[left+1] > arr[left] ? left+1 : left;
+            larget = arr[larget] > arr[index] ? larget : index;
+
+            if (larget == index){
+                break;
+            }
+            Swap.swap(arr,larget,index);//交互位置
+            index = larget;
+            left = index*2 + 1;
+        }
+    }
+}
