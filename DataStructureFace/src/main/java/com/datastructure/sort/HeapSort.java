b435192747108f80de4da2af84567e5b77848496
ming.chen1 <ming.chen1@sao.so>
2021-05-25 17:31:49 +0800

    完善堆排序

--- DataStructureFace/src/main/java/com/datastructure/sort/HeapSort.java
+++ DataStructureFace/src/main/java/com/datastructure/sort/HeapSort.java
@@ -19,10 +19,17 @@ public class HeapSort {
         for (int i = 0; i < arr.length; i++) {
             insertHeap(arr,i);
         }
+        int heapSize = arr.length;
+        Swap.swap(arr,0,--heapSize);
+        while (heapSize > 0){
+            heapify(arr,0,heapSize);
+            Swap.swap(arr,0,--heapSize);
+        }
     }
 
     /**
      * 插入进堆里面，如果是降序排序就采用大根堆，大根堆即为当前节点堆的左右孩子都小于当前节点
+     * 建立大根堆
      * @param arr
      * @param index
      */
@@ -33,7 +40,12 @@ public class HeapSort {
         }
     }
 
-
+    /**
+     * 对大根堆进行排序
+     * @param arr
+     * @param index
+     * @param heapSize
+     */
     public static void heapify(int[] arr,int index,int heapSize){
         //得到当前节点的左孩子
         int left = 2*index +1;
