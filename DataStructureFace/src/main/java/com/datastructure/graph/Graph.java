c7e30b8079c97acd0df99a36b47188d8b07f3f3c
cmcm <1446525549@qq.com>
2021-06-11 02:35:35 +0800

    新增图的深度优先遍历和广度优先遍历

---
+++ DataStructureFace/src/main/java/com/datastructure/graph/Graph.java
@@ -0,0 +1,169 @@
+package com.datastructure.graph;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * 表示图
+ */
+public class Graph {
+
+    private ArrayList<String> vertexList;//存储定点集合
+    private int[][] edges;//存储图对应的邻接矩阵
+    private int  numOfEdges;//表示边的个数
+    private boolean[] isVisited;
+
+    public Graph(int n){
+        vertexList = new ArrayList<>();
+        edges = new int[n][n];
+        numOfEdges = 0;
+        isVisited = new boolean[n];
+    }
+
+
+    /**
+     * 深度优先遍历图，简单的来说就是一条路直到走不下去了就开始回溯
+     * @param isVisited
+     * @param i
+     */
+    public void dfs(boolean[] isVisited,int i){
+        System.out.print(getValueByIndex(i)+"->");
+        isVisited[i] = true;
+        int w = getFirstNeighbor(i);
+        while (w != -1){
+            if (!isVisited[w]){
+                dfs(isVisited,w);
+            }
+            w = getNextNeighbor(i,w);
+        }
+    }
+
+    //对dfs重载，遍历所有的结点
+    public void dfs(){
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+    /**
+     * 广度优先算法
+     * @param i
+     */
+    private void bfs(boolean[] isVisited,int i){
+        int u;
+        int w;
+        System.out.printf(getValueByIndex(i)+"->");
+        isVisited[i] = true;
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.addLast(i);
+        while (!queue.isEmpty()){
+            u = queue.removeFirst();
+            w = getFirstNeighbor(u);
+            while (w != -1){
+                if (!isVisited[w]){
+                    System.out.printf(getValueByIndex(w)+"->");
+                    isVisited[w] = true;
+                    queue.addLast(w);
+                }
+                w = getNextNeighbor(u,w);
+            }
+        }
+    }
+
+    public void bfs(){
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]){
+                bfs(isVisited,i);
+            }
+        }
+    }
+    /**
+     * 得到第一个邻接结点的下标w
+     *
+     * @param index
+     * @return 如果存在返回对应的下标，否则返回-1
+     */
+    public int getFirstNeighbor(int index){
+        for (int j = 0; j < vertexList.size(); j++) {
+            if (edges[index][j] > 0){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接结点的下标来获取下一个邻接结点
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public int getNextNeighbor(int v1,int v2){
+        for (int j = v2+1; j < vertexList.size(); j++) {
+            if (edges[v1][j] > 0){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    public void showGraph(){
+        for (int[] link : edges) {
+            System.out.println(Arrays.toString(link));
+        }
+    }
+    /**
+     * 得到两个定点之间的权重
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public int getWeight(int v1,int v2){
+        return edges[v1][v2];
+    }
+    /**
+     * 对应位置的点
+     * @param i
+     * @return
+     */
+    public String getValueByIndex(int i){
+        return vertexList.get(i);
+    }
+    /**
+     * 返回边点个数
+     * @return
+     */
+    public int getNumOfEdges() {
+        return numOfEdges;
+    }
+
+    /**
+     * 返回节点点个数
+     * @return
+     */
+    public int getNumOfVertex(){
+        return vertexList.size();
+    }
+    /**
+     * 插入点
+     * @param vertex
+     */
+    public void insertVertex(String vertex){
+        vertexList.add(vertex);
+    }
+
+    /**
+     *
+     * @param v1 表示点点下表及第几个顶点
+     * @param v2
+     * @param weight
+     */
+    public void insertEdge(int v1,int v2,int weight){
+        edges[v1][v2] = weight;
+        edges[v2][v1] = weight;
+        numOfEdges++;
+    }
+}
---
+++ DataStructureFace/src/main/java/com/datastructure/graph/GraphMain.java
@@ -0,0 +1,36 @@
+package com.datastructure.graph;
+
+
+/**
+ * 深度优先遍历的算法步骤
+ * 1、访问初始节点v，并标记节点v为已访问
+ * 2、查找节点v的第一个邻接节点w
+ * 3、若w存在，则继续执行4，如果w不存在，则回到第一步，将从v的下一个结点继续
+ * 4、若w未被访问，对w进行深度优先遍历递归，则把w当做另一个v，然后进行步骤1，2，3
+ * 5查找结点v的w邻接结点，转到步骤3
+ */
+public class GraphMain {
+
+    public static void main(String[] args) {
+        int n = 5;
+        String Vertexs[] = {"A","B","C","D","E"};
+        Graph graph = new Graph(n);
+        for (String vertex : Vertexs) {
+            graph.insertVertex(vertex);
+        }
+        //添加边
+        //a-b a-c b-c b-d b-e
+        graph.insertEdge(0,1,1);//表示的是a-b这条边，权重为1
+        graph.insertEdge(0,2,1);
+        graph.insertEdge(1,2,1);
+        graph.insertEdge(1,3,1);
+        graph.insertEdge(1,4,1);
+
+        //显示邻接矩阵
+        graph.showGraph();
+//        System.out.println("图的深度遍历dfs");
+//        graph.dfs();
+        System.out.println("图的广度优先");
+        graph.bfs();
+    }
+}
